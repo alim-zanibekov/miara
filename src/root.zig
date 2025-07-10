75e9bc1bd3f042b1091891835ff369a9fe7f3a3d
alimzanibekov <zanibekov.alim@gmail.com>
2025-07-10 22:52:00 +0500

    add root module file

---
+++ src/root.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub usingnamespace @import("ef.zig");
+pub const filter = @import("filter.zig");
+pub const iterator = @import("iterator.zig");
+pub usingnamespace @import("bit.zig");
+pub usingnamespace @import("pthash.zig");
+pub const widow = @import("widow.zig");
+pub const symspell = @import("symspell.zig");
+pub const util = @import("util.zig");
+pub const fastd = @import("fastd.zig");
+pub const serde = @import("serde.zig");
+
+test {
+    std.testing.refAllDecls(@This());
+}
